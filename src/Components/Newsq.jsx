a6514d289d0ed6a907103b80de374cb9e669e232
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-05-25 17:33:45 +0300

    dices

--- src/Components/Newsq.jsx
+++ src/Components/Newsq.jsx
@@ -1,13 +1,23 @@
 function NewSq({ list }) {
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "flex-start",
+        flexWrap: "wrap",
+        gap: "20px",
+      }}
+    >
       {list.map((li) => (
         <div
           className={"dice-" + li.number}
-          style={{ color: li.color, fontSize: li.size + "px" }}
+          style={{
+            color: li.color,
+            fontSize: li.size + "px",
+          }}
         ></div>
       ))}
-    </>
+    </div>
   );
 }
 
